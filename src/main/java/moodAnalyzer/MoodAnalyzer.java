1141eb0ba1e7eedc8dd32bfc4d8d6066f61055b6
Surjyamani Duan <88813830+surjyamanidun@users.noreply.github.com>
2021-10-29 18:18:59 +0530

    Update MoodAnalyzer.java
    exception try pass

--- src/main/java/moodAnalyzer/MoodAnalyzer.java
+++ src/main/java/moodAnalyzer/MoodAnalyzer.java
@@ -1,5 +1,40 @@
 package moodAnalyzer;
 
-public class MoodAnalyzer {
 
+public class MoodAnalyzer {
+    private String message;
+
+    public MoodAnalyzer() {
+    }
+
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() throws MoodAnalysisException {
+        try {
+
+            if (this.message.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (Exception e) {
+            if (message == null)
+                throw new MoodAnalysisException("Invalid Mood", MoodAnalysisException.Exception_Type.NULL);
+            else
+                throw new MoodAnalysisException("Invalid Mood", MoodAnalysisException.Exception_Type.EMPTY);
+        }
+    }
+  public class MoodAnalysisException extends Exception {
+    public Exception_Type type;
+
+    public MoodAnalysisException(String message, Exception_Type type) {
+        super(message);
+        this.type = type;
+    }
+
+    public enum Exception_Type {
+        NULL, EMPTY;
+    }
+}
 }
