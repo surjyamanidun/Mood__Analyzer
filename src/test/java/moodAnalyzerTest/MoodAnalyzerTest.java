e039c47715a218aa09cd1fbb111a44642f0b9adf
Surjyamani Duan <88813830+surjyamanidun@users.noreply.github.com>
2021-10-29 18:11:19 +0530

    Update MoodAnalyzerTest.java

--- src/test/java/moodAnalyzerTest/MoodAnalyzerTest.java
+++ src/test/java/moodAnalyzerTest/MoodAnalyzerTest.java
@@ -1,5 +1,16 @@
 package moodAnalyzerTest;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MoodAnalyzerTest {
+    @Test
+    public void given_SadMood_Should_Return_Sad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.MoodAnalyzerr("I am In a Sad Mood");
+        Assert.assertEquals("SAD", mood);
+
+    }
 
 }
