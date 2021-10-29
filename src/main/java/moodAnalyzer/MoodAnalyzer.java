30ccc01e096c0b7a45082055b3834cb033a35636
Surjyamani Duan <88813830+surjyamanidun@users.noreply.github.com>
2021-10-29 18:12:41 +0530

    Update MoodAnalyzer.java
    analyse sad or happy

--- src/main/java/moodAnalyzer/MoodAnalyzer.java
+++ src/main/java/moodAnalyzer/MoodAnalyzer.java
@@ -1,5 +1,12 @@
 package moodAnalyzer;
-
 public class MoodAnalyzer {
+    public MoodAnalyzer() {
+    }
 
+    public String analyzerMood(String message) {
+        if (message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
 }
