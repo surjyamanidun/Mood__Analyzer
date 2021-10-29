3d63cc8eb68edd8def1d7f1a040ad5848a951579
Surjyamani Duan <88813830+surjyamanidun@users.noreply.github.com>
2021-10-29 18:23:02 +0530

    Update MoodAnalysisException.java
    hendal exception

--- src/main/java/moodAnalyzer/MoodAnalysisException.java
+++ src/main/java/moodAnalyzer/MoodAnalysisException.java
@@ -1,5 +1,11 @@
 package moodAnalyzer;
 
 public class MoodAnalysisException extends Exception {
+    public MoodAnalysisException(String message, Exception_Type type) {
+        super(message);
+    }
 
+    public enum Exception_Type {
+        NULL, EMPTY;
+    }
 }
