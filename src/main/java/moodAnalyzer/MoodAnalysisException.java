e58117e51910098d126fddb1b43ca544c4cd7b5a
Surjyamani <88813830+surjyamanidun@users.noreply.github.com>
2021-10-30 08:53:47 +0530

    Update MoodAnalysisException.java

--- src/main/java/moodAnalyzer/MoodAnalysisException.java
+++ src/main/java/moodAnalyzer/MoodAnalysisException.java
@@ -1,5 +1,15 @@
 package moodAnalyzer;
+package com.exception;
 
 public class MoodAnalysisException extends Exception {
+    public Exception_Type type;
 
+    public MoodAnalysisException(String message, Exception_Type type) {
+        super(message);
+        this.type = type;
+    }
+
+    public enum Exception_Type {
+        NULL, EMPTY;
+    }
 }
