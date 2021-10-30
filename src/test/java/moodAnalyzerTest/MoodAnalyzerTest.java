d9032a6f04cccdee61b7a824b1c13c687a8379ce
Surjyamani <88813830+surjyamanidun@users.noreply.github.com>
2021-10-30 08:56:03 +0530

    Update MoodAnalyzerTest.java

--- src/test/java/moodAnalyzerTest/MoodAnalyzerTest.java
+++ src/test/java/moodAnalyzerTest/MoodAnalyzerTest.java
@@ -1,5 +1,17 @@
 package moodAnalyzerTest;
 
-public class MoodAnalyzerTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class MoodAnalyzerTest {
+    @Test
+    public void given_NullMood_Should_Throw_MoodAnalysisException() {
+        MoodAnalyzer moodAnalyser = new MoodAnalyzer(null);
+        String mood;
+        try {
+            mood = moodAnalyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            Assert.assertEquals(MoodAnalysisException.Exception_Type.NULL,e.type);
+        }
+    }
 }
