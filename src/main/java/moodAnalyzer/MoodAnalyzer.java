af1a186b61a6210a696395dd9722809f7f28fbf9
Surjyamani <88813830+surjyamanidun@users.noreply.github.com>
2021-10-30 08:54:33 +0530

    Update MoodAnalyzer.java

--- src/main/java/moodAnalyzer/MoodAnalyzer.java
+++ src/main/java/moodAnalyzer/MoodAnalyzer.java
@@ -1,5 +1,27 @@
 package moodAnalyzer;
 
 public class MoodAnalyzer {
+    private String message;
 
+    public MoodAnalyzer() {
+    }
+
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() throws MoodAnalysisException {
+        try {
+
+            if (this.message.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (Exception e) {
+            if (message == null)
+                throw new MoodAnalysisException("Invalid Mood", MoodAnalysisException.Exception_Type.NULL);
+            else
+                throw new MoodAnalysisException("Invalid Mood", MoodAnalysisException.Exception_Type.EMPTY);
+        }
+    }
 }
